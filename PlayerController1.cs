c45e4fb278088c1300062b25b0f27221ebc80034
SGokilapriya <36201974+SGokilapriya@users.noreply.github.com>
2020-08-28 08:28:56 +0530

    Add files via upload

---
+++ PlayerController1.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController1 : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI score;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+    private int count;
+    private int scoree;
+    private float movementX;
+    private float movementY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        scoree = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        score.text = "Score" + scoree.ToString();
+        if(count == 16)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            scoree = scoree + 5;
+            SetCountText();
+        }
+
+    }
+     
+}
